185671f5e348fd2d2244da3380046d2bd00f0484
HongJow <hoongjow@gmail.com>
2019-07-09 17:53:04 +0900

    start commit

---
+++ src/main/java/org/ajou/realcoding/summonerposition/summonerposition/SummonerPositionApplication.java
@@ -0,0 +1,13 @@
+package org.ajou.realcoding.summonerposition.summonerposition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummonerPositionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummonerPositionApplication.class, args);
+	}
+
+}
