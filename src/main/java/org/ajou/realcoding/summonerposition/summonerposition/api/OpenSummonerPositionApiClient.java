6788dd3917a74179207a3b2da986f178a61949db
haejin <bae05030@gmail.com>
2019-07-11 14:25:53 +0900

    Add api Client get summoner position by summoner name

---
+++ src/main/java/org/ajou/realcoding/summonerposition/summonerposition/api/OpenSummonerPositionApiClient.java
@@ -0,0 +1,35 @@
+package org.ajou.realcoding.summonerposition.summonerposition.api;
+
+import org.ajou.realcoding.summonerposition.summonerposition.domain.SummonerId;
+import org.ajou.realcoding.summonerposition.summonerposition.domain.SummonerPosition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OpenSummonerPositionApiClient {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final String apiKey = "RGAPI-c60c314c-9253-48c3-a118-0b9a57900086";
+    private final String SummonerRequseByNameUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{SummonerName}?api_key={apiKey}";
+    private final String SummonerPositionByIdUrl = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}?api_key={apiKey}";
+
+    private SummonerId summonerId;
+
+    public void getSummonerId(String SummonerName){
+       summonerId = restTemplate.exchange(SummonerRequseByNameUrl, HttpMethod.GET,null,SummonerId.class,SummonerName,apiKey).getBody();
+       getSummonerPosition(summonerId.getId());
+    }
+
+    public SummonerPosition getSummonerPosition(String summonerid){
+        SummonerPosition summonerPosition = restTemplate.exchange(SummonerRequseByNameUrl, HttpMethod.GET,null,SummonerPosition.class,summonerid,apiKey).getBody();
+        return summonerPosition;
+    }
+
+
+
+
+}
