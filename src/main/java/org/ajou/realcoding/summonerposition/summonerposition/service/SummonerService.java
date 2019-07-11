071d8c8439091a656562f2cf15cb4730bac73caa
HongJow <hoongjow@gmail.com>
2019-07-11 16:11:43 +0900

    change service for test

--- src/main/java/org/ajou/realcoding/summonerposition/summonerposition/service/SummonerService.java
+++ src/main/java/org/ajou/realcoding/summonerposition/summonerposition/service/SummonerService.java
@@ -12,6 +12,14 @@ public class SummonerService {
     OpenSummonerPositionApiClient openSummonerPositionApiClient;
 
     public SummonerPosition getSummonerPositionByName(String summonerName) {
-        return openSummonerPositionApiClient.getSummonerId(summonerName);
+        SummonerPosition summonerPosition = openSummonerPositionApiClient.getSummonerPosition(summonerName);
+        /*
+        if(findSummonerIdByName(summonerName)){
+            updateSummonerPositionBySummonerName(summonerPosition);
+        } else{
+            createSummonerPosition(summonerPosition);
+        }
+         */
+        return summonerPosition;
     }
 }
