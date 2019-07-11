748104d6e7d1625554ea3af6961f81a5e0bca3d3
mo <mhmrm2040@naver.com>
2019-07-11 15:46:02 +0900

    create insertSummonerPosition Method

--- src/main/java/org/ajou/realcoding/summonerposition/summonerposition/repository/SummonerPositionRepository.java
+++ src/main/java/org/ajou/realcoding/summonerposition/summonerposition/repository/SummonerPositionRepository.java
@@ -1,12 +1,25 @@
 package org.ajou.realcoding.summonerposition.summonerposition.repository;
 
+import org.ajou.realcoding.summonerposition.summonerposition.domain.SummonerPosition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.Query;
 
 public class SummonerPositionRepository {
 
     @Autowired
     MongoTemplate mongoTemplate;
+
+    public SummonerPosition insertSummonerPosition(SummonerPosition summonerPosition){
+        return mongoTemplate.save(summonerPosition);
+    }
+
+
+
+
+
+
+
 
 
 
