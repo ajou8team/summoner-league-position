4b898f1c96c55682f518c6b329734b0d83201146
mo <mhmrm2040@naver.com>
2019-07-11 15:20:18 +0900

    add annotation Restcontroller

--- src/main/java/org/ajou/realcoding/summonerposition/summonerposition/controller/SummonerPositionController.java
+++ src/main/java/org/ajou/realcoding/summonerposition/summonerposition/controller/SummonerPositionController.java
@@ -5,7 +5,9 @@ import org.ajou.realcoding.summonerposition.summonerposition.service.SummonerSer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class SummonerPositionController {
 
     @Autowired
