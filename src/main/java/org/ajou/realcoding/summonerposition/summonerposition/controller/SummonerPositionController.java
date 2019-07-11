efa81d2629378c9932f66c4ab4ad271536aafcb8
mo <mhmrm2040@naver.com>
2019-07-11 14:26:14 +0900

    add import domain

--- src/main/java/org/ajou/realcoding/summonerposition/summonerposition/controller/SummonerPositionController.java
+++ src/main/java/org/ajou/realcoding/summonerposition/summonerposition/controller/SummonerPositionController.java
@@ -1,5 +1,6 @@
 package org.ajou.realcoding.summonerposition.summonerposition.controller;
 
+import org.ajou.realcoding.summonerposition.summonerposition.domain.SummonerPosition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
