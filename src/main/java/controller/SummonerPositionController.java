0835b52446a22367823d28cdcfd8d39aec9c05a6
mo <mhmrm2040@naver.com>
2019-07-11 14:14:37 +0900

    add SummonerService summonerService

--- src/main/java/controller/SummonerPositionController.java
+++ src/main/java/controller/SummonerPositionController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class SummonerPositionController {
+
+    SummonerService summonerService;
+
+
+
 }
