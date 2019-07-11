99a066b5cf8f6fdda818dcb3ae7be5cabad88e17
mo <mhmrm2040@naver.com>
2019-07-11 15:51:37 +0900

    add findSummornerPositionBySummonerName Method

--- src/main/java/org/ajou/realcoding/summonerposition/summonerposition/repository/SummonerPositionRepository.java
+++ src/main/java/org/ajou/realcoding/summonerposition/summonerposition/repository/SummonerPositionRepository.java
@@ -3,7 +3,8 @@ package org.ajou.realcoding.summonerposition.summonerposition.repository;
 import org.ajou.realcoding.summonerposition.summonerposition.domain.SummonerPosition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 public class SummonerPositionRepository {
 
@@ -12,6 +13,13 @@ public class SummonerPositionRepository {
 
     public SummonerPosition insertSummonerPosition(SummonerPosition summonerPosition){
         return mongoTemplate.save(summonerPosition);
+    }
+
+    public SummonerPosition findSummonerPositionBySummornerName(String summornerName){
+        Query query = new Query();
+        query.addCriteria(Criteria.where("name").is(summornerName));
+
+        return mongoTemplate.findOne(query, SummonerPosition.class);
     }
 
 
