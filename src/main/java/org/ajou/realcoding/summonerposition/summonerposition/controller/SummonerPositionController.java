57328d9e1773d90e47c5b00b0bafb956183ebd13
mo <mhmrm2040@naver.com>
2019-07-11 14:24:25 +0900

    put in org.ajou.realcoding.summonerposition.summonerposition

--- src/main/java/org/ajou/realcoding/summonerposition/summonerposition/controller/SummonerPositionController.java
+++ src/main/java/org/ajou/realcoding/summonerposition/summonerposition/controller/SummonerPositionController.java
@@ -1,4 +1,4 @@
-package controller;
+package org.ajou.realcoding.summonerposition.summonerposition.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
