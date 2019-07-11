ce8621a46d8fcd38d6cee35616a5058ab5ead3cd
mo <mhmrm2040@naver.com>
2019-07-11 15:28:45 +0900

    add MongoTemplate and annotation

--- src/main/java/org/ajou/realcoding/summonerposition/summonerposition/repository/SummonerPositionRepository.java
+++ src/main/java/org/ajou/realcoding/summonerposition/summonerposition/repository/SummonerPositionRepository.java
@@ -1,4 +1,13 @@
 package org.ajou.realcoding.summonerposition.summonerposition.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
 public class SummonerPositionRepository {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+
+
 }
