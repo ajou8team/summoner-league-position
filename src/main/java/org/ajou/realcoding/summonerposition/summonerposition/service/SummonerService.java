805e2a0893ab39541a62151ddc28ca31681fdcd3
HongJow <hoongjow@gmail.com>
2019-07-13 18:43:05 +0900

    put summonerPosition data to repository in service

--- src/main/java/org/ajou/realcoding/summonerposition/summonerposition/service/SummonerService.java
+++ src/main/java/org/ajou/realcoding/summonerposition/summonerposition/service/SummonerService.java
@@ -3,27 +3,29 @@ package org.ajou.realcoding.summonerposition.summonerposition.service;
 import org.ajou.realcoding.summonerposition.summonerposition.api.OpenSummonerPositionApiClient;
 import org.ajou.realcoding.summonerposition.summonerposition.domain.SummonerId;
 import org.ajou.realcoding.summonerposition.summonerposition.domain.SummonerPosition;
+import org.ajou.realcoding.summonerposition.summonerposition.repository.SummonerPositionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Iterator;
 import java.util.Set;
 
 @Service
 public class SummonerService {
-
     @Autowired
     OpenSummonerPositionApiClient openSummonerPositionApiClient;
+    @Autowired
+    SummonerPositionRepository summonerPositionRepository;
 
     public Set<SummonerPosition> getSummonerPositionByName(String summonerName) {
         SummonerId summonerId = openSummonerPositionApiClient.getSummonerId(summonerName);
         Set<SummonerPosition> summonerPositionSet = openSummonerPositionApiClient.getSummonerPosition(summonerId.getId());
-        /*
-        if(findSummonerIdByName(summonerName)){
-            updateSummonerPositionBySummonerName(summonerPosition);
-        } else{
-            createSummonerPosition(summonerPosition);
+
+        Iterator<SummonerPosition> IterSummonerPosition = summonerPositionSet.iterator();
+        while(IterSummonerPosition.hasNext()){
+            summonerPositionRepository.insertSummonerPosition(IterSummonerPosition.next());
         }
-         */
+
         return summonerPositionSet;
     }
 }
