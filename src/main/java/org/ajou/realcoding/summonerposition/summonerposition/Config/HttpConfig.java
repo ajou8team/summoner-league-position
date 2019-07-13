d637d08fb44e52fc383e2bcd9ecb535de566f0af
HongJow <hoongjow@gmail.com>
2019-07-13 14:27:59 +0900

    set indentation

--- src/main/java/org/ajou/realcoding/summonerposition/summonerposition/Config/HttpConfig.java
+++ src/main/java/org/ajou/realcoding/summonerposition/summonerposition/Config/HttpConfig.java
@@ -8,9 +8,8 @@ import org.springframework.web.client.RestTemplate;
 public class HttpConfig {
 
     @Bean
-public RestTemplate createRestTemple()
-{
-
+    public RestTemplate createRestTemple()
+    {
     return new RestTemplate();
-}
+    }
 }
